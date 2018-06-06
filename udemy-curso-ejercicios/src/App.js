101efee2f9affd2a7a2c8403750f3d7ccf014620
David G <david.garcia@scmspain.com>
2018-06-06 23:12:16 +0200

    adds component PokemonList and JSON data file

---
+++ udemy-curso-ejercicios/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import PokemonList from "./exercises/props/pokemonList";
+import data from "./data/pokemons.json";
+/**
+ * Para ejecutar el primer ejercicio has de importar lo siguiente:
+ * import PokemonList from './exercises/props/pokemonList'
+ * import data from "./data/pokemons.json"
+ *
+ * Luego, en el render, deberías utilizar el componente así
+ *   render () {
+      return (
+        <div className="App">
+          <PokemonList pokemons={data}/>
+        </div>
+      );
+    }
+ */
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PokemonList pokemons={data} />
+      </div>
+    );
+  }
+}
+
+export default App;
